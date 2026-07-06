f26f87d611380629cb4216609692976a60f59bab
Lakshmi5321 <Lakshminalla532@gmail.com>
2026-07-06 17:32:24 +0530

    Adding ContactPage and MailPage to Github repository

--- src/test/java/Pages/ContactPage.java
+++ src/test/java/Pages/ContactPage.java
@@ -1,6 +1,107 @@
 package Pages;
 
-public class ContactPage
-{
+import Base.BaseUtil;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
 
+public class ContactPage extends BaseUtil
+{
+    public ContactPage(AndroidDriver driver)
+    {
+        super(driver);
+        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+    }
+
+    By viewContacts = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='1']/child::android.widget.TextView[@text='View contacts']");
+    By createContact = By.xpath("//android.widget.TextView[@text='Create contact']");
+    By firstName = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='1']/descendant::android.widget.FrameLayout[@index='0']/child::android.widget.EditText[@index='0']");
+    By lastName = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='2']/descendant::android.widget.FrameLayout[@index='0']/child::android.widget.EditText[@index='0']");
+    By companyName = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='3']/child::android.widget.EditText[@index='0']");
+    By phoneNumber = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='4']/child::android.widget.EditText[@index='0']");
+    By save = By.xpath("//android.view.View[@index='0']/child::android.view.View[@index='3']/child::android.widget.TextView[@index='0']");
+    By title = By.xpath("//android.widget.TextView[@resource-id='com.google.android.contacts:id/large_title']");
+
+    By editIcon = By.id("com.google.android.dialer:id/open_search_bar_text_view");
+    By searchBar = By.xpath("com.google.android.dialer:id/open_search_view_edit_text");
+    By searchContact = By.id("com.google.android.dialer:id/open_search_view_edit_text");
+    By searchResult = By.id("com.google.android.dialer:id/photo");
+    By deleteBar = By.xpath("//android.widget.TextView[@text='Delete']");
+
+    public void clickViewContacts()
+    {
+        waitUtil.waitForVisibility(viewContacts).click();
+    }
+
+    public void clickCreateContact()
+    {
+        waitUtil.waitForClickable(createContact).click();
+    }
+
+    public void sendFirstName(String fname)
+    {
+        waitUtil.waitForVisibility(firstName).sendKeys(fname);
+    }
+
+    public void sendLastName(String lname)
+    {
+        waitUtil.waitForVisibility(lastName).sendKeys(lname);
+    }
+
+    public void sendCompanyName(String cname)
+    {
+        waitUtil.waitForVisibility(companyName).sendKeys(cname);
+    }
+
+    public void sendPhoneNumber(String phone)
+    {
+        waitUtil.waitForVisibility(phoneNumber).sendKeys(phone);
+    }
+
+    public void clickSave()
+    {
+        waitUtil.waitForClickable(save).click();
+    }
+
+    public boolean checkTitle(String value)
+    {
+        return waitUtil.checkForTextPresent(title,value);
+    }
+
+    public void clickEditIcon()
+    {
+        waitUtil.waitForClickable(editIcon).click();
+    }
+
+    public void clickSearchBar()
+    {
+        waitUtil.waitForVisibility(searchBar).click();
+    }
+
+    public void clickSearchContact()
+    {
+        waitUtil.waitForVisibility(searchContact).click();
+    }
+
+    public void clickSearchResult()
+    {
+        waitUtil.waitForVisibility(searchResult).click();
+    }
+
+    public void clickDeleteBar()
+    {
+        waitUtil.waitForVisibility(deleteBar).click();
+    }
+
+    public void clickDeleteByAlert()
+    {
+        waitUtil.checkForAlertPresent();
+    }
+
+    public void performSwipeUp()
+    {
+        gestureUtil.swipe(488,2107,501,304);
+    }
 }
+
--- src/test/java/Pages/MailPage.java
+++ src/test/java/Pages/MailPage.java
@@ -1,6 +1,185 @@
 package Pages;
 
-public class MailPage
-{
+import Base.BaseUtil;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
 
+public class MailPage extends BaseUtil
+{
+    public MailPage(AndroidDriver driver)
+    {
+        super(driver);
+        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+    }
+
+    By gotIt = By.id("com.google.android.gm:id/welcome_tour_got_it");
+    By takeMeToGmail = By.id("com.google.android.gm:id/action_done");
+    By allow = By.id("com.android.permissioncontroller:id/permission_allow_button");
+    By gotIt_HomePage = By.id("com.google.android.gm:id/next_button");
+    By compose = By.id("com.google.android.gm:id/compose_button");
+    By toMail = By.xpath("//android.view.ViewGroup[@index='0']/child::android.widget.EditText[@index='0']");
+    By mailSuggestion = By.xpath("//android.widget.RelativeLayout[@index='0']/descendant::android.widget.ImageView[@index='0']");
+    By subjectMail = By.xpath("//android.widget.LinearLayout[@index='0']/child::android.widget.EditText[@index='0']");
+    By send = By.id("com.google.android.gm:id/send");
+    By description = By.id("com.google.android.gm:id/description_text");
+
+    By meet = By.xpath("//android.widget.FrameLayout[@index='1' and @content-desc='Meet']");
+    By cancel = By.id("android:id/button2");
+    By newMeeting = By.id("com.google.android.gm:id/new_meeting");
+    By getAMeetingLinkToShare = By.id("com.google.android.gm:id/get_meeting_link_button");
+    By shareInvite = By.id("com.google.android.gm:id/joining_info_share_button");
+    By gmail = By.xpath("//android.widget.RelativeLayout[@index='0']/descendant::android.widget.TextView[@text='Gmail']");
+    By close = By.id("com.google.android.gm:id/joining_info_close_button");
+
+    By navigationMenu = By.xpath("//android.view.ViewGroup[@index='0']/child::android.widget.ImageButton[@index='0']");
+    By settings = By.xpath("//android.widget.LinearLayout[@index='18']");
+    By generalSettings = By.xpath("//android.widget.LinearLayout[@index='0']/child::android.widget.TextView[@index='0']");
+
+    By theme = By.xpath("//android.widget.RelativeLayout[@index='0']/child::android.widget.TextView[@index='0' and @text='Theme']");
+    By light = By.xpath("//android.widget.ListView[@index='0']/child::android.widget.CheckedTextView[@index='0']");
+    By themeText = By.xpath("//android.widget.LinearLayout[@index='0']/descendant::android.widget.TextView[@index='1' and @text='Light']");
+
+    By manageNotifications = By.xpath("//android.widget.LinearLayout[@index='2']");
+    By allGmailNotificationsToggle = By.id("android:id/switch_widget");
+    By allGmailNotificationsText = By.xpath("//android.widget.LinearLayout[@index='0']/descendant::android.widget.TextView[@text='All Gmail notifications']");
+
+    public void clickGotIt()
+    {
+        waitUtil.waitForVisibility(gotIt).click();
+    }
+
+    public void clickTakeMeToGmail()
+    {
+        waitUtil.waitForVisibility(takeMeToGmail).click();
+    }
+
+    public void clickAllow()
+    {
+        waitUtil.checkForAlertPresent();
+    }
+
+    public void clickGotItInHomePage()
+    {
+        waitUtil.waitForVisibility(gotIt_HomePage).click();
+    }
+
+    public void clickCompose()
+    {
+        waitUtil.waitForClickable(compose).click();
+    }
+
+    public void enterToMail(String to)
+    {
+        waitUtil.waitForVisibility(toMail).clear();
+        waitUtil.waitForVisibility(toMail).sendKeys(to);
+    }
+
+    public void clickSuggestion()
+    {
+        waitUtil.waitForVisibility(mailSuggestion).click();
+    }
+
+    public void enterSubject(String subject)
+    {
+        waitUtil.waitForVisibility(subjectMail).clear();
+        waitUtil.waitForVisibility(subjectMail).sendKeys(subject);
+    }
+
+    public void clickSend()
+    {
+        waitUtil.waitForVisibility(send).click();
+    }
+
+    public boolean checkDescription(String value)
+    {
+        return waitUtil.checkForTextPresent(description,value);
+    }
+
+    public void clickMeet()
+    {
+        waitUtil.waitForVisibility(meet).click();
+    }
+
+    public void clickCancel()
+    {
+        waitUtil.waitForClickable(cancel).click();
+    }
+
+    public void clickNewMeeting()
+    {
+        waitUtil.waitForVisibility(newMeeting).click();
+    }
+
+    public void clickGetAMeetingLinkToShare()
+    {
+        waitUtil.waitForClickable(getAMeetingLinkToShare).click();
+    }
+
+    public void clickShareInvite()
+    {
+        waitUtil.waitForClickable(shareInvite).click();
+    }
+
+    public void clickGmail()
+    {
+        waitUtil.waitForClickable(gmail).click();
+    }
+
+    public void clickClose()
+    {
+        waitUtil.waitForClickable(close).click();
+    }
+
+    public void clickNavigationMenu() throws InterruptedException
+    {
+        waitUtil.waitForVisibility(navigationMenu).click();
+        Thread.sleep(2000);
+    }
+
+    public void clickSettings()
+    {
+        waitUtil.waitForVisibility(settings).click();
+    }
+
+    public void clickGeneralSettings()
+    {
+        waitUtil.waitForVisibility(generalSettings).click();
+    }
+
+    public void clickTheme()
+    {
+        waitUtil.waitForClickable(theme).click();
+    }
+
+    public void clickLight()
+    {
+        waitUtil.waitForVisibility(light).click();
+    }
+
+    public boolean checkThemeText(String text)
+    {
+        return waitUtil.checkForTextPresent(themeText,text);
+    }
+
+    public void clickManageNotifications()
+    {
+        waitUtil.waitForClickable(manageNotifications).click();
+    }
+
+    public boolean checkAllGmailNotificationsText(String text)
+    {
+        return waitUtil.checkForTextPresent(allGmailNotificationsText,text);
+    }
+
+    public void doubleTapAction()
+    {
+        gestureUtil.doubleTap(waitUtil.waitForClickable(allGmailNotificationsToggle));
+    }
+
+    public void swipeAction()
+    {
+        gestureUtil.swipe(343,2170,302,686);
+    }
 }
