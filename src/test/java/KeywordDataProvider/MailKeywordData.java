b7e675a9dcde9031b80de1dce8c84a3b864c5f17
Lakshmi5321 <Lakshminalla532@gmail.com>
2026-07-06 11:25:18 +0530

    Adding KeywordUtil, ContactKeywordData and MailKeywordData to Github repository

--- src/test/java/KeywordDataProvider/MailKeywordData.java
+++ src/test/java/KeywordDataProvider/MailKeywordData.java
@@ -1,4 +1,49 @@
 package KeywordDataProvider;
 
-public class MailKeywordData {
+import Utils.KeywordUtil;
+import java.io.IOException;
+import java.util.List;
+
+public class MailKeywordData
+{
+    public List<String> getSendMail_KeywordData() throws IOException
+    {
+        KeywordUtil keywordUtil = new KeywordUtil();
+        String path = System.getProperty("user.dir")+"/KeywordData/Keyword Data.xlsx";
+        String sheetName = "Mail";
+        String column = "Send Mail";
+
+        return keywordUtil.getKeywordData(path,sheetName,column);
+    }
+
+    public List<String> getSendMeet_KeywordData() throws IOException
+    {
+       KeywordUtil keywordUtil = new KeywordUtil();
+       String path = System.getProperty("user.dir")+"/KeywordData/Keyword Data.xlsx";
+       String sheetName = "Mail";
+       String column = "Send Meet Link";
+
+       return keywordUtil.getKeywordData(path,sheetName,column);
+    }
+
+    public List<String> getCheckThemeText_KeywordData() throws IOException
+    {
+       KeywordUtil keywordUtil = new KeywordUtil();
+       String path = System.getProperty("user.dir")+"/KeywordData/Keyword Data.xlsx";
+       String sheetName = "Mail";
+       String column = "Check Theme Text";
+
+       return keywordUtil.getKeywordData(path,sheetName,column);
+    }
+
+    public List<String> getCheckManageNotificationsText_KeywordData() throws IOException
+    {
+        KeywordUtil keywordUtil = new KeywordUtil();
+        String path = System.getProperty("user.dir")+"/KeywordData/Keyword Data.xlsx";
+        String sheetName = "Mail";
+        String column = "Check Manage Notifications Text";
+
+        return keywordUtil.getKeywordData(path,sheetName,column);
+    }
+
 }
--- src/test/java/Utils/KeywordUtil.java
+++ src/test/java/Utils/KeywordUtil.java
@@ -1,4 +1,74 @@
 package Utils;
 
-public class KeywordUtil {
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeywordUtil
+{
+    public List<String> getKeywordData(String path,String sheetName,String column) throws IOException
+    {
+        FileInputStream fis = new FileInputStream(path);
+        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFSheet sheet = workbook.getSheet(sheetName);
+
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(0).getPhysicalNumberOfCells();
+        List<String> data =  new ArrayList<>();
+        int colNum = 0;
+
+        for(int i=0;i<1;i++)
+        {
+            boolean flag = false;
+
+            Row row = sheet.getRow(i);
+            if(row == null)
+            {
+                continue;
+            }
+
+            for(int j=0;j<cols;j++)
+            {
+                Cell cell = row.getCell(i);
+                if(cell != null && !cell.toString().trim().isEmpty() && column.equals(cell.toString()))
+                {
+                    colNum = j;
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag)
+            {
+                break;
+            }
+        }
+
+        for(int i=1;i<rows;i++)
+        {
+            Row row = sheet.getRow(i);
+            if(row == null)
+            {
+                continue;
+            }
+
+            for(int j=0;j<cols;j++)
+            {
+                if(colNum == j)
+                {
+                    Cell cell = row.getCell(j);
+                    if(cell != null && !cell.toString().trim().isEmpty())
+                    {
+                        data.add(cell.toString());
+                    }
+                }
+            }
+        }
+        return data;
+    }
 }
